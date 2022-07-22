9937f5dfc2e11bf82aafd19eb7b14dc7177b8e99
Sodynoizz_TH <86451119+Sodynoizz@users.noreply.github.com>
2022-07-22 18:36:17 +0700

    Add variable keys to `secret/keys.py` files
    Add information to input `TOKEN`, `CLIENT_ID`, `CLIENT_SECRET`

--- secret/keys.py
+++ secret/keys.py
@@ -1,4 +1,4 @@
-API_ENDPOINT = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
-BOT_TOKEN = ""
+API_ENDPOINT = 'https://discord.com/api/v10'
+CLIENT_ID = 'Input CLIENT_ID here'
+CLIENT_SECRET = 'Input CLIENT_SECRET here'
+BOT_TOKEN = 'Input BOT_TOKEN here'
